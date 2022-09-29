6e12f09994961b6a29f1e1a6da03f4058bad8136
S010MON <leon@bdebnath.com>
2022-09-29 19:04:03 +0200

    try again to get reception

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,17 +1,22 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial HC12(11, 10);
+const byte Rxd = 4;
+const byte Txd = 5;
+
+SoftwareSerial HC12(Txd, Rxd);
 
 void setup() 
 {
   Serial.begin(9600);
   HC12.begin(9600);
+  Serial.write("Started\n");
 }
 
 void loop() 
 {
   while(HC12.available())
   {
+    Serial.write("read");
     Serial.write(HC12.read());
   }
   while(Serial.available())
