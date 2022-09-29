ccead8f33a27bd738ea8f85620321e776ecadfbe
S010MON <leon@bdebnath.com>
2022-09-29 20:39:12 +0200

    found data sheet!

--- srf_transmitter/srf_transmitter.ino
+++ srf_transmitter/srf_transmitter.ino
@@ -1,12 +1,24 @@
 void setup() 
 {
   pinMode(8, OUTPUT);
+  pinMode(13, OUTPUT);
   digitalWrite(8, HIGH);
   Serial.begin(115200);
   Serial.println("Hello World");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+    if(Serial.available())
+    {
+      char c = Serial.read();
+      if( c == '0')
+      {
+        digitalWrite(13, LOW);
+      }
+      else if ( c == '1')
+      {
+        digitalWrite(13, HIGH);
+      }
+    }
 }
