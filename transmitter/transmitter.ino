268f031296b43861b20aab2d4b29c67a27c3b4e8
S010MON <leon@bdebnath.com>
2022-09-29 18:05:05 +0200

    HC12 serial tranciever

---
+++ transmitter/transmitter.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial HC12(1, 0);
+
+void setup() 
+{
+  Serial.begin(9600);
+  HC12.begin(9600);
+}
+
+void loop() 
+{
+  while(HC12.available())
+  {
+    Serial.write(HC12.read());
+  }
+  while(Serial.available())
+  {
+    HC12.write(Serial.read());
+  }
+}
