fdf06e187671c1c1979871dad1d4b00e4bee66ea
S010MON <leon_debnath@hotmail.co.uk>
2022-10-01 19:01:40 +0200

    Add LED flashing interrupt

--- ground_station/ground_station.ino
+++ ground_station/ground_station.ino
@@ -2,26 +2,43 @@
 #define L_STICK_Y A0
 #define R_STICK_X A3
 #define R_STICK_Y A2
+#define LED 13
 
 int l_x;
 int l_y;
 int r_x;
 int r_y;
-
+bool led;
+long flashRate = 1000; // milliseconds
+unsigned long timer;
 
 void setup() 
 {
   Serial.begin(9600);
-
+  
+  pinMode(LED, OUTPUT);
   pinMode(L_STICK_X, INPUT);
   pinMode(L_STICK_Y, INPUT);
   pinMode(R_STICK_X, INPUT);
   pinMode(R_STICK_Y, INPUT);
 
+  for(int i = 0; i < 3; i++)
+  {
+    digitalWrite(LED, HIGH);
+    delay(100);
+    digitalWrite(LED, LOW);
+    delay(100);
+  }
+
+  digitalWrite(LED, HIGH);
+  led = true;
+  timer =  millis();
 }
 
 void loop()
 {
+  checkTimer();
+  
   l_y = map(analogRead(L_STICK_Y), 0, 1023, 0, 180);
   r_x = map(analogRead(R_STICK_X), 0, 1023, 0, 180);
   r_y = map(analogRead(R_STICK_Y), 0, 1023, 0, 180);
@@ -53,5 +70,23 @@ void loop()
     Serial.write('p');
     Serial.write(l_y);
     Serial.write('\n');
+  }
+}
+
+void checkTimer()
+{
+  long timeElapsed = millis() - timer;
+  if(timeElapsed > flashRate)
+  {
+      if(led)
+      {
+        digitalWrite(LED, LOW);
+      }
+      else
+      {
+        digitalWrite(LED, HIGH);
+      }
+      led = !led;
+      timer = millis();
   }
 }
